4934bc9749eef7fe9be9b50b3af7671643e0182d
sjoleee <cent7425@gmail.com>
2022-12-22 00:26:15 +0900

    feat: :sparkles: 상수 파일 분리

---
+++ src/constants/index.ts
@@ -0,0 +1,20 @@
+export enum CELL_TYPE {
+  NORMAL = -1,
+  QUESTION = -2,
+  FLAG = -3,
+  QUESTION_MINE = -4,
+  FLAG_MINE = -5,
+  MINE = -6,
+  OPENED = 0,
+}
+
+export enum GAME_STATUS {
+  READY = "READY",
+  PLAYING = "PLAYING",
+  WIN = "WIN",
+  LOSE = "LOSE",
+}
+
+export const ROW = 10;
+export const COL = 10;
+export const MINE = 10;
