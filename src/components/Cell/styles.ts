e826ee461b9086fa707a1f8584e3ad55b1522274
sjoleee <cent7425@gmail.com>
2022-12-30 23:25:58 +0900

    feat: :lipstick: 스타일 작성

--- src/components/Cell/styles.ts
+++ src/components/Cell/styles.ts
@@ -4,5 +4,5 @@ export const Button = styled.button<{ isOpen: boolean; isBomb: boolean }>`
   font-size: 12px;
   width: 30px;
   height: 30px;
-  background-color: ${({ isOpen, isBomb }) => (isOpen ? "white" : isBomb ? "red" : "gray")};
+  background-color: ${({ isOpen, isBomb }) => (isOpen ? "lightGray" : isBomb ? "red" : "gray")};
 `;
--- src/components/Control/index.tsx
+++ src/components/Control/index.tsx
@@ -16,74 +16,84 @@ const Control = () => {
   }, []);
 
   return (
-    <>
-      <button
-        type="button"
-        onClick={() => {
-          dispatch(resize({ rowCount, colCount, mineCount }));
-        }}
-      >
-        Reset
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          dispatch(resize({ rowCount: 8, colCount: 8, mineCount: 10 }));
-        }}
-      >
-        Beginner
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          dispatch(resize({ rowCount: 16, colCount: 16, mineCount: 40 }));
-        }}
-      >
-        Intermediate
-      </button>
-      <button
-        type="button"
-        onClick={() => {
-          dispatch(resize({ rowCount: 16, colCount: 32, mineCount: 100 }));
-        }}
-      >
-        Expert
-      </button>
+    <S.Container>
       <div>
-        board size :
-        <S.Input
-          type="number"
-          value={customCol}
-          onChange={(e) => {
-            setCustomCol(Number(e.target.value));
+        <S.Button
+          type="button"
+          reset
+          onClick={() => {
+            dispatch(resize({ rowCount, colCount, mineCount }));
           }}
-        />{" "}
-        X
-        <S.Input
-          type="number"
-          value={customRow}
-          onChange={(e) => {
-            setCustomRow(Number(e.target.value));
+        >
+          Reset
+        </S.Button>
+      </div>
+      <S.LevelContainer>
+        LEVEL
+        <S.Button
+          type="button"
+          onClick={() => {
+            dispatch(resize({ rowCount: 8, colCount: 8, mineCount: 10 }));
           }}
-        />{" "}
-        mine count:
-        <S.Input
-          type="number"
-          value={customMine}
-          onChange={(e) => {
-            setCustomMine(Number(e.target.value));
+        >
+          Beginner
+        </S.Button>
+        <S.Button
+          type="button"
+          onClick={() => {
+            dispatch(resize({ rowCount: 16, colCount: 16, mineCount: 40 }));
           }}
-        />
-        <button
+        >
+          Intermediate
+        </S.Button>
+        <S.Button
+          type="button"
+          onClick={() => {
+            dispatch(resize({ rowCount: 16, colCount: 32, mineCount: 100 }));
+          }}
+        >
+          Expert
+        </S.Button>
+      </S.LevelContainer>
+      <S.CustomContainer>
+        <div>
+          board size :
+          <S.Input
+            type="number"
+            value={customCol}
+            onChange={(e) => {
+              setCustomCol(Number(e.target.value));
+            }}
+          />{" "}
+          X
+          <S.Input
+            type="number"
+            value={customRow}
+            onChange={(e) => {
+              setCustomRow(Number(e.target.value));
+            }}
+          />
+        </div>
+        <div>
+          mine count:
+          <S.Input
+            type="number"
+            value={customMine}
+            onChange={(e) => {
+              setCustomMine(Number(e.target.value));
+            }}
+          />
+        </div>
+        <S.Button
           type="button"
           onClick={() => {
             dispatch(resize({ rowCount: customRow, colCount: customCol, mineCount: customMine }));
           }}
         >
           Custom
-        </button>
-      </div>
-    </>
+        </S.Button>
+      </S.CustomContainer>
+    </S.Container>
   );
 };
 
